16f0e1c0eb2e6c70d86f266c228f8df6a79ab772
Nikolay Voronov <nkvoronov@gmail.com>
2015-05-27 01:00:31 +0300

    update and fixes

--- packages/addons/service/multimedia/tvheadenda/package.mk
+++ packages/addons/service/multimedia/tvheadenda/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="tvheadenda"
-PKG_VERSION="4.1.41"
-PKG_REV="36"
+PKG_VERSION="4.1.77"
+PKG_REV="37"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.tvheadend.org"
@@ -41,7 +41,7 @@ makeinstall_target() {
 }
 
 addon() {
-  TVHEADEND_DIR=$ROOT/$BUILD/tvheadend-ef5b43a
+  TVHEADEND_DIR=$ROOT/$BUILD/tvheadend-22b2ed9
   
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
     cp -P $TVHEADEND_DIR/.$TARGET_NAME/build.linux/tvheadend $ADDON_BUILD/$PKG_ADDON_ID/bin
--- packages/custom/services/tvheadend-service/package.mk
+++ packages/custom/services/tvheadend-service/package.mk
@@ -19,8 +19,8 @@
 ################################################################################
 
 PKG_NAME="tvheadend-service"
-PKG_VERSION="4.1.41"
-PKG_REV="36"
+PKG_VERSION="4.1.77"
+PKG_REV="37"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.lonelycoder.com/hts/tvheadend_overview.html"
@@ -43,7 +43,7 @@ makeinstall_target() {
 }
 
 post_install() {
-  TVHEADEND_DIR=$ROOT/$BUILD/tvheadend-ef5b43a
+  TVHEADEND_DIR=$ROOT/$BUILD/tvheadend-22b2ed9
 
   mkdir -p $INSTALL/usr/bin
     cp -P $TVHEADEND_DIR/.$TARGET_NAME/build.linux/tvheadend $INSTALL/usr/bin
