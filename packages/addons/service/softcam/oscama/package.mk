ab5ceac537a1de5585c5dfa451efb4ecc42cbddb
Nikolay Voronov <nkvoronov@gmail.com>
2015-07-18 16:06:17 +0300

    updates

--- packages/addons/service/softcam/oscama/package.mk
+++ packages/addons/service/softcam/oscama/package.mk
@@ -20,8 +20,8 @@
 ################################################################################
 
 PKG_NAME="oscama"
-PKG_VERSION="10895"
-PKG_REV="47"
+PKG_VERSION="10896"
+PKG_REV="48"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.streamboard.tv/oscam/wiki"
@@ -46,7 +46,7 @@ makeinstall_target() {
 }
 
 addon() {
-  OSCAM_DIR=$ROOT/$BUILD/oscam-10895
+  OSCAM_DIR=$ROOT/$BUILD/oscam-10896
   PCSCLITE_DIR=$ROOT/$BUILD/pcsc-lite-1.8.11
 
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
--- packages/custom/services/vdr-plugins/vdr-plugin-skindesigner/package.mk
+++ packages/custom/services/vdr-plugins/vdr-plugin-skindesigner/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="vdr-plugin-skindesigner"
-PKG_VERSION="9611291"
+PKG_VERSION="d9c5219"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
