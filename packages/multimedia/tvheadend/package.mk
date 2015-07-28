c4a1277055504645e1cdeeefb486803ca8a71c25
Nikolay Voronov <nkvoronov@gmail.com>
2015-07-29 02:24:12 +0300

    updates

--- packages/multimedia/tvheadend/package.mk
+++ packages/multimedia/tvheadend/package.mk
@@ -19,9 +19,9 @@
 ################################################################################
 
 PKG_NAME="tvheadend"
-PKG_VERSION="0916e52"
-PKG_VERSIONA="4.1.360"
-PKG_REV="46"
+PKG_VERSION="d9cf931"
+PKG_VERSIONA="4.1.361"
+PKG_REV="47"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.lonelycoder.com/hts/tvheadend_overview.html"
