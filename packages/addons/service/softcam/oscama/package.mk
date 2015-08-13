54833cb93f7cc97ac0ed7eda3a7280cdbbbaf803
Nikolay Voronov <nkvoronov@gmail.com>
2015-08-14 00:59:15 +0300

    update

--- packages/addons/service/softcam/oscama/package.mk
+++ packages/addons/service/softcam/oscama/package.mk
@@ -20,8 +20,8 @@
 ################################################################################
 
 PKG_NAME="oscama"
-PKG_VERSION="10937"
-PKG_REV="43"
+PKG_VERSION="10940"
+PKG_REV="44"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.streamboard.tv/oscam/wiki"
@@ -46,7 +46,7 @@ makeinstall_target() {
 }
 
 addon() {
-  OSCAM_DIR=$ROOT/$BUILD/oscam-10937
+  OSCAM_DIR=$ROOT/$BUILD/oscam-10940
   PCSCLITE_DIR=$ROOT/$BUILD/pcsc-lite-1.8.11
 
   mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/bin
--- packages/multimedia/oscam/package.mk
+++ packages/multimedia/oscam/package.mk
@@ -20,8 +20,8 @@
 ################################################################################
 
 PKG_NAME="oscam"
-PKG_VERSION="10937"
-PKG_REV="43"
+PKG_VERSION="10940"
+PKG_REV="44"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.streamboard.tv/oscam/wiki"
