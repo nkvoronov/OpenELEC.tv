6f34cde26453136606d827701cdbadff02188a90
Nikolay Voronov <nkvoronov@gmail.com>
2015-10-20 05:09:25 +0300

    update

--- packages/addons/service/multimedia/tvheadenda/package.mk
+++ packages/addons/service/multimedia/tvheadenda/package.mk
@@ -17,8 +17,8 @@
 ################################################################################
 
 PKG_NAME="tvheadenda"
-PKG_VERSION="4.1.639"
-PKG_REV="75"
+PKG_VERSION="4.1.661"
+PKG_REV="76"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.tvheadend.org"
--- packages/custom/multimedia/oscam-service/package.mk
+++ packages/custom/multimedia/oscam-service/package.mk
@@ -20,8 +20,8 @@
 ################################################################################
 
 PKG_NAME="oscam-service"
-PKG_VERSION="11058"
-PKG_REV="72"
+PKG_VERSION="11059"
+PKG_REV="73"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.streamboard.tv/oscam/wiki"
