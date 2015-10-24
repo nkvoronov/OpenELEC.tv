2689aaccc2a396021690ca120035fa8ff1057e49
Nikolay Voronov <nkvoronov@gmail.com>
2015-10-24 19:50:12 +0300

    update

--- packages/custom/multimedia/oscam/package.mk
+++ packages/custom/multimedia/oscam/package.mk
@@ -20,8 +20,8 @@
 ################################################################################
 
 PKG_NAME="oscam"
-PKG_VERSION="11061"
-PKG_REV="74"
+PKG_VERSION="11062"
+PKG_REV="75"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.streamboard.tv/oscam/wiki"
