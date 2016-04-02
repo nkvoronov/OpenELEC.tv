8dd6ddf39d1c2e8019c49b6505b72c2e224bc024
Nikolay Voronov <nkvoronov@gmail.com>
2016-04-03 00:09:32 +0300

    update

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -39,7 +39,7 @@ case "$KODIPLAYER_DRIVER" in
     PKG_KEEP_CHECKOUT="yes"
     ;;
   *)
-    #PKG_VERSION="3ee62f7"
+    #PKG_VERSION="17dd50a"
     PKG_VERSION="be20e8a"
     PKG_GIT_URL="https://github.com/xbmc/xbmc.git"
     PKG_GIT_BRANCH="Jarvis"
