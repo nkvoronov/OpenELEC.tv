52b22d8767167ef6994cfb0bec68e4cca8c7700a
Nikolay Voronov <nkvoronov@gmail.com>
2016-05-10 19:00:49 +0300

    update

--- packages/addons/official/kodi-addons/pvr.hts/package.mk
+++ packages/addons/official/kodi-addons/pvr.hts/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="pvr.hts"
-PKG_VERSION="7a3b3a3"
+PKG_VERSION="ffc970d"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
