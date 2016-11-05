1652d4c9c98060ff44dbf799d875fefbf5fa1bf7
Nikolay Voronov <nkvoronov@gmail.com>
2016-11-05 04:02:47 +0200

    update

--- packages/custom/mediacentre/kodi-repository-yellowdragon/package.mk
+++ packages/custom/mediacentre/kodi-repository-yellowdragon/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="kodi-repository-yellowdragon"
-PKG_VERSION="16.1.064"
+PKG_VERSION="16.1.065"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
